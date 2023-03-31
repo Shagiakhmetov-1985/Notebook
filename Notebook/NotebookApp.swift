46061ce3e09faa93f1488bb8931d6bbc7797e1ff
Marat <Shagiakhmetov2499@gmail.com>
2023-03-31 23:11:45 +0300

    Initial Commit

---
+++ Notebook/NotebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotebookApp.swift
+//  Notebook
+//
+//  Created by Marat Shagiakhmetov on 31.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct NotebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
